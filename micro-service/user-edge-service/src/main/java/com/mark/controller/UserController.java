9a07ec5244c1a0a1697da977daf450ad40c12993
e <18009838166@163.com>
2020-03-09 12:30:10 +0800

    refactoring the code and rebuild docker images based on these software of spring

--- micro-service/user-edge-service/src/main/java/com/mark/controller/UserController.java
+++ micro-service/user-edge-service/src/main/java/com/mark/controller/UserController.java
@@ -11,6 +11,7 @@ import org.apache.thrift.TException;
 import org.apache.tomcat.util.buf.HexUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
